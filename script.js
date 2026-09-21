90da9978d67b20a9f30fb56a42aab4a0c02c1b53
Lia <aliyahrenomeron7@gmail.com>
2026-09-21 13:32:56 -0400

    created the journal page

---
+++ script.js
@@ -0,0 +1,11 @@
+const journalBtn = document.getElementById("journal-btn");
+
+const goBackBtn = document.getElementById("go-back-btn");
+
+journalBtn.addEventListener("click", function() {
+    window.location.href = "../journal.html";
+})
+
+goBackBtn.addEventListener("click", function() {
+    // wtf
+})
